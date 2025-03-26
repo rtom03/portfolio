1da8d006c673448e8f68c7e9bfbe0026e454c29f
rtom03 <zuckm709@gmail.com>
2025-03-26 21:50:01 +0100

    customization of names

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -106,7 +106,7 @@ const SidebarHeader = () => {
   return (
     <div className="flex space-x-2">
       <Image
-        src="/rtom.jpg"
+        src="/im.jpg"
         alt="Avatar"
         height="40"
         width="40"
--- src/constants/products.tsx
+++ src/constants/products.tsx
@@ -6,8 +6,40 @@ import sidefolioMoonbeam from "public/images/sidefolio-moonbeam.png";
 import sidefolioMoonbeam2 from "public/images/sidefolio-moonbeam-2.png";
 import sidefolioTailwindMasterKit from "public/images/sidefolio-tailwindmasterkit.png";
 import sidefolioTailwindMasterKit2 from "public/images/sidefolio-tailwindmasterkit-2.png";
+import chimeln from "public/images/chimeln.png";
 
 export const products = [
+  {
+    href: "https://chimeln.vercel.app/",
+    title: "Chimeln - Chat-app",
+    description:
+      "A beautiful and comprehensive projects with the help of Tailwind CSS,MERN stack and socket.io implementation for real time chat based applications.",
+    thumbnail: chimeln,
+    images: [chimeln, chimeln],
+
+    stack: ["Nextjs", "Tailwindcss"],
+    slug: "tailwindmasterkit",
+    content: (
+      <div>
+        <p>
+          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
+          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
+          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
+          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
+          non cupidatat duis esse velit ut culpa et.{" "}
+        </p>
+        <p>
+          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
+          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
+          sit aute fugiat incididunt nostrud consequat proident fugiat id.
+          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
+          reprehenderit. Cillum Lorem veniam eu magna exercitation.
+          Reprehenderit adipisicing minim et officia enim et veniam Lorem
+          excepteur velit adipisicing et Lorem magna.
+        </p>{" "}
+      </div>
+    ),
+  },
   {
     href: "https://blog-xi-nine-14.vercel.app",
     title: "Aceternity",
@@ -47,37 +79,6 @@ export const products = [
     images: [sidefolioAlgochurn, sidefolioAlgochurn2],
     stack: ["Nextjs", "Tailwindcss"],
     slug: "algochurn",
-    content: (
-      <div>
-        <p>
-          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
-          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
-          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
-          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
-          non cupidatat duis esse velit ut culpa et.{" "}
-        </p>
-        <p>
-          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
-          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
-          sit aute fugiat incididunt nostrud consequat proident fugiat id.
-          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
-          reprehenderit. Cillum Lorem veniam eu magna exercitation.
-          Reprehenderit adipisicing minim et officia enim et veniam Lorem
-          excepteur velit adipisicing et Lorem magna.
-        </p>{" "}
-      </div>
-    ),
-  },
- 
-  {
-    href: "https://tailwindmasterkit.com",
-    title: "Tailwind Master Kit",
-    description:
-      "A beautiful and comprehensive Tailwind CSS components library for building modern websites and applications.",
-    thumbnail: sidefolioTailwindMasterKit,
-    images: [sidefolioTailwindMasterKit, sidefolioTailwindMasterKit2],
-    stack: ["Nextjs", "Tailwindcss"],
-    slug: "tailwindmasterkit",
     content: (
       <div>
         <p>
